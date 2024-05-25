a425787eee65cff55a7f1da64a48ccaaa6c4a033
manuel <47613275+elettro00@users.noreply.github.com>
2024-05-25 16:37:51 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,9 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB1Fj9hHYh7lZMje_DbFdVnhvadJCINkrU",
-  authDomain: "esp32-monitoraggiosensori.firebaseapp.com",
-  projectId: "esp32-monitoraggiosensori",
-  storageBucket: "esp32-monitoraggiosensori.appspot.com",
-  messagingSenderId: "371820595202",
-  appId: "1:371820595202:web:13082103809a3596e97f1b",
-  databaseUrl: "https://esp32-monitoraggiosensori-default-rtdb.europe-west1.firebasedatabase.app"
+  //your firebase config
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const rtDb = getDatabase(app, "https://esp32-monitoraggiosensori-default-rtdb.europe-west1.firebasedatabase.app");
+export const rtDb = getDatabase(app, //your firebase database link "");
